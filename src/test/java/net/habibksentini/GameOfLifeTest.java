03d863b4ea11ea2fb068ab57cfafb1ce043ceba3
habibks <ksentinhabib@gmail.com>
2017-04-04 00:41:53 +0200

    Implement third rule: Any live cell with two or three live neighbours lives on to the next generation

--- src/test/java/net/habibksentini/GameOfLifeTest.java
+++ src/test/java/net/habibksentini/GameOfLifeTest.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.collection.IsIterableContainingInAnyOrder.containsInAnyOrder;
 
 public class GameOfLifeTest {
@@ -26,10 +25,8 @@ public class GameOfLifeTest {
         assertThat(gameOfLife.getCells(), containsInAnyOrder(new Cell(3, 2), new Cell(4, 2)));
     }
 
-
     @Test
     public void any_live_cell_with_more_than_three_live_neighbours_dies() {
-
         Set<Cell> cells = new HashSet<>();
         cells.add(new Cell(2, 2));
         cells.add(new Cell(2, 3));
@@ -41,7 +38,21 @@ public class GameOfLifeTest {
         gameOfLife.nextGeneration();
 
         assertThat(gameOfLife.getCells(), containsInAnyOrder(new Cell(2, 2), new Cell(2, 3), new Cell(4, 2)));
+    }
 
+    @Test
+    public void any_live_cell_with_two_or_three_live_neighbours_lives() {
+
+        Set<Cell> cells = new HashSet<>();
+        cells.add(new Cell(2, 2));
+        cells.add(new Cell(2, 3));
+        cells.add(new Cell(3, 2));
+        cells.add(new Cell(3, 3));
+        GameOfLife gameOfLife = new GameOfLife(cells);
+
+        gameOfLife.nextGeneration();
+
+        assertThat(gameOfLife.getCells(), containsInAnyOrder(new Cell(2, 2), new Cell(2, 3), new Cell(3, 2), new Cell(3, 3)));
     }
 
 }
